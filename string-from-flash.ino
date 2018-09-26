1f9588c23603350876342ca69f459ae5a3f912e5
Sosnus <sosnus15@gmail.com>
2018-09-26 19:01:25 +0200

    Update string-from-flash.ino

--- string-from-flash.ino
+++ string-from-flash.ino
@@ -1,12 +1,15 @@
 const char page_start2[] PROGMEM  = {"<!DOCTYPE html><html lang=\"en\"><head><title>NAZWA STRONY</title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\">  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script><script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\"></script><script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\"></script></head><body><div class=\"container\"><h2>OPIS PROJEKTU</h2><p>COKOLWIEK</p><div class=\"card-deck\">"};
-
-void setup() {
-  Serial.begin(9600);
-  for (int i = 0; i <= strlen_P(page_start2); i++)
+void printFromFlash(char *source)
+{
+  for (int i = 0; i <= strlen_P(source); i++)
   {
-    char    myChar =  pgm_read_byte_near(page_start2 + i);
+    char myChar =  pgm_read_byte_near(source + i);
     Serial.print(myChar);
   }
+}
+void setup() {
+  Serial.begin(9600);
+  printFromFlash(page_start2);
 }
 
 void loop() {
